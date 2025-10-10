c706578f40205d4dbca7e61c4e39c55432c1f7a1
Lucas <lucasignaciosilva18@gmail.com>
2025-10-09 22:36:23 -0300

    practica 02

--- csv/data_ej.py
+++ csv/data_ej.py
@@ -1,13 +1,21 @@
 import csv
 
 lista_equipos = [
-    {"id",2,""}
+    {"id":1, "nombre":"Super Compu","categoria":"notebook", "estado": "funcional", "valor": 500},
+    {"id":2, "nombre":"Mega Compu","categoria":"notebook", "estado": "funcional", "valor": 700},
+    {"id":3, "nombre":"Ultra Compu","categoria":"notebook", "estado": "funcional", "valor": 1000}
 ]
 
 with open("equipos.csv", "w", newline = "")as archivo: 
     writer = csv.DictWriter(archivo, fieldnames=["id","nombre","categoria","estado","valor"])
 
     writer.writeheader()
-    writer.writerow()
+    for equipo in lista_equipos:
+        writer.writerow(equipo)
+
+with open("equipos.csv", "r") as archivo:
+    reader = csv.DictReader(archivo)
+    for linea in reader:
+        print(linea)
 
 
