9799c4f4e3f9c6fb33e2fb49622b92b926e0f643
Lucas <lucasignaciosilva18@gmail.com>
2025-10-12 20:56:38 -0300

    listas,diccionarios,colores

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from colorama import init, Fore, Back, Style
+import random
 #! ESTO ES PARA PROBAR COSAS NOMAS
 
 init()
@@ -26,3 +27,26 @@ def main():
     print("\nMensaje:\n" + Fore.LIGHTWHITE_EX + Back.LIGHTBLUE_EX + Style.BRIGHT +"Racing Club de Avellaneda" + Style.RESET_ALL)
 
 main()
+
+
+print("Diccionario")
+diccionario = {"saludo": "Hola", "despedida":"Chau"}
+diccionario[2] = "dos"
+#diccionario["nombre_pj"] = input("Ingrese su nombre: ")
+#print(Fore.LIGHTGREEN_EX + Style.BRIGHT + diccionario["nombre_pj"] + Style.RESET_ALL)
+#print(diccionario)
+
+# print("\nDiccionario2")
+# diccionario2 = {"nombre":Fore.LIGHTCYAN_EX + input("Ingrese su nombre: " + Style.RESET_ALL),
+#                 "apellido":Fore.GREEN + input("Ingrese su apellido: ") + Style.RESET_ALL}
+
+# print(diccionario2["nombre"])
+# print(diccionario2["apellido"])
+
+personaje = {
+    "pj1":{"nombre":input("Ingrese su nombre: "),"color":Fore.LIGHTGREEN_EX,"ataque":int(input("Ingrese su daño de ataque: "))},
+    "pj2":{"nombre":input("Ingrese su nombre: "),"color":Fore.LIGHTGREEN_EX,"ataque":int(input("Ingrese su daño de ataque: "))}
+}
+for clave, pj in personaje.items():
+    print(pj["color"] + pj["nombre"] + Style.RESET_ALL + f" tiene {pj['ataque']} de ataque")
+    #print(pj["nombre"] + f" tiene {pj["ataque"]} de ataque")
